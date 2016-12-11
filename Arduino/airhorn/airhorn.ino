1e5134c3ed93363b51d552f94f5d3323ddc2f5ca
Theo Kanning <theokanning@gmail.com>
2016-12-11 16:08:31 -0600

    First version of arduino code

--- Arduino/airhorn/airhorn.ino
+++ Arduino/airhorn/airhorn.ino
@@ -1,10 +1,31 @@
+const int maxVolume = 255;
+const float weight = 0.1f;
+const int period = 20; //50Hz
+
+const int buttonPin = 5; //pick the correct pin later
+
+int volume = 0;
+
 void setup() {
+  pinMode(buttonPin, INPUT);
   Serial.begin(115200);
 }
 
 void loop() {
-  if(Serial.available())
-    {
-        Serial.write(Serial.read());    //send what has been received
-    }
+  updateVolume();
+  Serial.println(volume, DEC);
+  delay(20);
 }
+
+void updateVolume(){
+  int input;
+  if(digitalRead(buttonPin) == HIGH) {
+    input = maxVolume;
+  } else {
+    input = 0;
+  }
+
+  volume += round((input - volume)/weight);
+  
+}
+
